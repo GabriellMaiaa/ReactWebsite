857b1d10e4615c3cdd1aaa929e4c4befec81513b
Gabriel Maia <gribeiromaia736@gmail.com>
2023-03-29 11:09:32 -0300

    add outras partes

--- src/pages/HomePage/Data.js
+++ src/pages/HomePage/Data.js
@@ -9,7 +9,52 @@ export const homeObject = {
     headLine:'Lidere as vendas onlines de todo o seu nicho' , 
     primary:true , 
     buttonLabel:'Comece Agora!',
-    img: require('../../images/png1.png'),
+    img: require('../../images/png11.png'),
+    alt:'Imagem',
+    start:''
+}
+export const homeObjectTwo = {
+    lightBg:true, 
+    imgStart:'' , 
+    lightTopLine:false , 
+    lightTextDesc:false , 
+    lightText:true  , 
+    description:'Nós ajudamos as Empresas a expadirem suas receitas. Nosso time contém diversos especialistas que lhe ajudarão a alcançar seus objetivos.', 
+    topLine:'Maia Enterprises' ,
+    headLine:'Lidere as vendas onlines de todo o seu nicho' , 
+    primary:false , 
+    buttonLabel:'Comece Agora!',
+    img: require('../../images/png11.png'),
+    alt:'Imagem',
+    start:''
+}
+export const homeObjectThree = {
+    lightBg:false, 
+    imgStart:'' , 
+    lightTopLine:true , 
+    lightTextDesc:true , 
+    lightText:true  , 
+    description:'Nós ajudamos as Empresas a expadirem suas receitas. Nosso time contém diversos especialistas que lhe ajudarão a alcançar seus objetivos.', 
+    topLine:'Maia Enterprises' ,
+    headLine:'Lidere as vendas onlines de todo o seu nicho' , 
+    primary:true , 
+    buttonLabel:'Comece Agora!',
+    img: require('../../images/png11.png'),
+    alt:'Imagem',
+    start:''
+}
+export const homeObjectFour = {
+    lightBg:false, 
+    imgStart:'' , 
+    lightTopLine:true , 
+    lightTextDesc:true , 
+    lightText:true  , 
+    description:'Nós ajudamos as Empresas a expadirem suas receitas. Nosso time contém diversos especialistas que lhe ajudarão a alcançar seus objetivos.', 
+    topLine:'Maia Enterprises' ,
+    headLine:'Lidere as vendas onlines de todo o seu nicho' , 
+    primary:true , 
+    buttonLabel:'Comece Agora!',
+    img: require('../../images/png11.png'),
     alt:'Imagem',
     start:''
 }
--- src/pages/HomePage/Home.js
+++ src/pages/HomePage/Home.js
@@ -1,11 +1,14 @@
 import React from 'react'
 import InfoSection from '../../components/InfoSection/InfoSection'
-import { homeObject } from './Data'
+import { homeObject, homeObjectFour, homeObjectThree, homeObjectTwo } from './Data'
 
 const Home = () => {
   return (
     <>
       <InfoSection {...homeObject}/>
+      <InfoSection {...homeObjectTwo}/>
+      <InfoSection {...homeObjectThree}/>
+      <InfoSection {...homeObjectFour}/>
     </>
   )
 }
